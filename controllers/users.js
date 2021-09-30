ce7f474fca731139e631805baed545cf094d2bc9
Lee Berny Gifwandi <leesmacbookpro@macbook-pro.myfiosgateway.com>
2021-09-30 15:48:34 -0400

    fourth commit

--- controllers/users.js
+++ controllers/users.js
@@ -7,12 +7,13 @@ const User = require('../models/user.js');
 // New (registration page)
 
 // Create (registration route)
+// Create (registration route)
 userRouter.post('/', (req, res) => {
     //overwrite the user password with the hashed password, then pass that in to our database
-    req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(12));
+    req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
 
     User.create(req.body, (error, createdUser) => {
-        res.send(createdUser);
+        res.redirect('/');
     });
 });
 
--- server.js
+++ server.js
@@ -3,10 +3,18 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const mongoose = require('mongoose');
+const session = require('express-session');
 
 // Middleware
 // Body parser middleware: give us access to req.body
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+    session({
+        secret: process.env.SECRET,
+        resave: false,
+        saveUninitialized: false
+    }));
 
 // Routes / Controllers
 const userController = require('./controllers/users');
