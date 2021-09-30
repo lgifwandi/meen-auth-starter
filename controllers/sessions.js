5a90234ba4baf53d135d0f3ba9990132f4139205
Lee Berny Gifwandi <leesmacbookpro@macbook-pro.myfiosgateway.com>
2021-09-30 16:24:03 -0400

    sixth commit

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -7,6 +7,11 @@ const User = require('../models/user.js');
 // New (login page)
 
 // Delete (logout route)
+sessionsRouter.delete('/', (req, res) => {
+    req.session.destroy((error) => {
+        res.redirect('/');
+    });
+})
 
 // Create (login route)
 sessionsRouter.post('/', (req, res) => {
